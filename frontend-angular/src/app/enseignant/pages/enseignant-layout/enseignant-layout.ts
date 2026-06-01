519354dddbe7e46923e95c80b81616a104d3600b
NatZz9 <Nathan.hotellier@utbm.fr>
2026-06-01 17:34:49 +0200

    enseigant-layout

--- frontend-angular/src/app/enseignant/pages/enseignant-layout/enseignant-layout.ts
+++ frontend-angular/src/app/enseignant/pages/enseignant-layout/enseignant-layout.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainNavComponent } from '../../../general/main-nav/main-nav';
 
 @Component({
   selector: 'app-enseignant-layout',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  imports: [RouterOutlet, MainNavComponent],
+  templateUrl: './enseignant-layout.html',
+  styleUrl: './enseignant-layout.css',
 })
-export class EnseignantLayout {}
+export class EnseignantLayout {
+  realUserRole: string = 'admin';
+}
